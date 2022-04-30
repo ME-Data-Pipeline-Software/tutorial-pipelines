8432cd3c62c3146e37b7dae12b4539091e9bf06c
jmcvey3 <jmcvey3@users.noreply.github.com>
2022-04-29 17:25:26 -0700

    tutorial

---
+++ pipelines/ncei_arctic_cruise_example/check_data.py
@@ -0,0 +1,8 @@
+# %%
+import xarray as xr
+
+ds = xr.open_dataset(
+    "../../storage/root/data/arctic_ocean.ncei_arctic_cruise_example.a1/arctic_ocean.ncei_arctic_cruise_example.a1.20150112.000000.nc"
+)
+
+# %%
