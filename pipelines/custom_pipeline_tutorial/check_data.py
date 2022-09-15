4a4ce8a5b876ae4d75eeaac98bffe8d512d29f3d
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2022-09-14 19:21:27 -0700

    Updates for workshop

--- pipelines/custom_pipeline_tutorial/check_data.py
+++
@@ -1,8 +0,0 @@
-# %%
-import xarray as xr
-
-ds = xr.open_dataset(
-    "../../storage/root/data/arctic.custom_pipeline_tutorial.b1/arctic.custom_pipeline_tutorial.b1.20150112.000000.nc"
-)
-
-# %%
--- pipelines/ncei_arctic_cruise_example/test/test_pipeline.py
+++ pipelines/ncei_arctic_cruise_example/test/test_pipeline.py
@@ -9,7 +9,7 @@ def test_ncei_arctic_cruise_example_pipeline():
     pipeline = config.instantiate_pipeline()
 
     test_file = "pipelines/ncei_arctic_cruise_example/test/data/input/arctic_ocean.sample_data.csv"
-    expected_file = "pipelines/ncei_arctic_cruise_example/test/data/expected/arctic_ocean.ncei_arctic_cruise_example.a1.20150112.000000.nc'"
+    expected_file = "pipelines/ncei_arctic_cruise_example/test/data/expected/arctic_ocean.ncei_arctic_cruise_example.a1.20150112.000000.nc"
 
     dataset = pipeline.run([test_file])
     expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
